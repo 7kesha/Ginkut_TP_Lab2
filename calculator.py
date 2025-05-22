21f3804bb26eab1f24bf4990edba5df8797a8502
7kesha <21fi.p.hynkut@std.npu.edu.ua>
2025-05-22 09:15:07 +0300

    :)

--- calculator.py
+++ calculator.py
@@ -10,7 +10,5 @@ if __name__ == "__main__":
     num2 = 7
     print(f"{num1} + {num2} = {add(num1, num2)}")
     print(f"{num1} - {num2} = {subtract(num1, num2)}")
-    # Додаємо виклик нової функції
     print(f"{num1} * {num2} = {multiply(num1, num2)}")
-    # Змінюємо повідомлення
-    print("Виправлено текст повідомлення у main.")  # Змінений той самий рядок!
+    print("Виправлено текст повідомлення у main.")  
